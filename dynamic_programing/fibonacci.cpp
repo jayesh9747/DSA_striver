c696b1ad5eebc32ed3a48df568632bd762961b3f
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2024-02-20 02:08:06 +0530

    fibbonaci

--- dynamic_programing/fibonacci.cpp
+++ dynamic_programing/fibonacci.cpp
@@ -12,9 +12,40 @@ int f(int n){
 
 //memoisation 
 int mem(int n){
-    if
+    if(n <= 1) return n;
+    vector<int>dp(n+1,-1);
+
+    if(dp[n] != -1) return dp[n];
+
+    return dp[n] = mem(n-1)+ mem(n-2);
 }
 
+// tabulation 
+int tab(int n){
+    vector<int>dp(n+1, 0);
+    dp[0]= 0;
+    dp[1]= 1;
+
+    for(int i = 2 ; i<= n ; i++){
+        dp[i] = dp[i-1]+dp[i-2];
+    }
+
+    return dp[n];
+}
+
+//optimized tabulation 
+int otab(int n){
+    if( n <= 1 ) return n;
+    int prev2 = 0;
+    int prev =1;
+
+    for(int i = 2 ; i< n ; i++){
+        int curi = prev + prev2;
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
 
 int main(){
     int n ;
